877530dd0c3df85da46faf7bb3e149821bef10ca
VarelaTechDev <varela.tech.dev@gmail.com>
2024-05-07 22:21:35 -0400

    almost working

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import com.example.Application;
+import com.example.dto.AssertionFinishRequest;
+import com.example.dto.AssertionStartResponse;
 import com.example.dto.RegistrationFinishRequest;
 import com.example.dto.RegistrationStartResponse;
 import com.example.entity.Credentials;
@@ -10,14 +13,19 @@ import com.example.repository.UserRepository;
 import com.example.utils.BytesUtil;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.yubico.webauthn.AssertionRequest;
+import com.yubico.webauthn.AssertionResult;
+import com.yubico.webauthn.FinishAssertionOptions;
 import com.yubico.webauthn.FinishRegistrationOptions;
 import com.yubico.webauthn.RegistrationResult;
 import com.yubico.webauthn.RelyingParty;
+import com.yubico.webauthn.StartAssertionOptions;
 import com.yubico.webauthn.StartRegistrationOptions;
 import com.yubico.webauthn.data.AuthenticatorTransport;
 import com.yubico.webauthn.data.ByteArray;
 import com.yubico.webauthn.data.PublicKeyCredentialCreationOptions;
 import com.yubico.webauthn.data.UserIdentity;
+import com.yubico.webauthn.exception.AssertionFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -29,6 +37,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.SecureRandom;
 import java.util.Base64;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.Optional;
 import java.util.SortedSet;
@@ -41,6 +52,8 @@ public class Controller {
     private final JpaRepositoryCredential jpaRepositoryCredential;
     @Autowired
     private UserRepository userRepository;
+
+    private final Cache<String, AssertionStartResponse> assertionCache;
 
     @Autowired
     private RelyingParty relyingParty;
@@ -55,6 +68,8 @@ public class Controller {
         this.random = new SecureRandom();
         this.registrationCache = Caffeine.newBuilder().maximumSize(1000)
                 .expireAfterAccess(5, TimeUnit.MINUTES).build();
+        this.assertionCache = Caffeine.newBuilder().maximumSize(1000)
+                .expireAfterAccess(5, TimeUnit.MINUTES).build();
     }
 
 
@@ -65,13 +80,14 @@ public class Controller {
 
     // createUser?username=exampleUser
     @PostMapping("/createUser")
-    public ResponseEntity<User> createUser(@RequestParam String username) {
+    public ResponseEntity<User> createUser (@RequestParam String username) {
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new RuntimeException("User already exists with username: " + username);
+        }
         User user = new User();
         user.setUsername(username);
-        User savedUser = userRepository.save(user);
-        log.info("User created: {}", savedUser.getUsername());
-        log.info("User id: {}", savedUser.getUserId());
-        return ResponseEntity.ok(savedUser);
+        userRepository.save(user);
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping("/register/start")
@@ -145,5 +161,47 @@ public class Controller {
 
 
         return "success";
+    }
+
+    @PostMapping("/assertion/start")
+    public AssertionStartResponse start(@RequestParam String username) {
+        byte[] assertionId = new byte[16];
+        this.random.nextBytes(assertionId);
+
+        String assertionIdBase64 = Base64.getEncoder().encodeToString(assertionId);
+        AssertionRequest assertionRequest = this.relyingParty.startAssertion(StartAssertionOptions.builder()
+                .username(username)
+                .build());
+
+        AssertionStartResponse response = new AssertionStartResponse(assertionIdBase64, assertionRequest);
+
+        this.assertionCache.put(response.getAssertionId(), response);
+        System.out.println(response);
+        return response;
+    }
+
+    @PostMapping("/assertion/finish")
+    public boolean finish(@RequestBody AssertionFinishRequest finishRequest,
+                          HttpServletRequest request, HttpServletResponse response) {
+
+        AssertionStartResponse startResponse = this.assertionCache
+                .getIfPresent(finishRequest.getAssertionId());
+        this.assertionCache.invalidate(finishRequest.getAssertionId());
+
+        try {
+            log.info("BEFORE FINISH ASSERTION");
+            log.info("finishRequest: " + finishRequest);
+            AssertionResult result = this.relyingParty.finishAssertion(
+                    FinishAssertionOptions.builder().request(startResponse.getAssertionRequest())
+                            .response(finishRequest.getCredential()).build());
+            log.info("AFTER");
+            System.out.println(result);
+            return true;
+        }
+        catch (AssertionFailedException e) {
+            log.error("Assertion failed", e);
+        }
+
+        return false;
     }
 }
